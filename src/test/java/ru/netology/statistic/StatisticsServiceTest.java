6a1f00ab56533ed2b8df99ab64ca22df283a0cfc
Yulia Khrenenko <romanova_jv@mail.ru>
2020-07-19 14:12:35 +0300

    Tests added

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,28 @@ class StatisticsServiceTest {
 
     assertEquals(expected, actual);
   }
+
+  @Test
+  void findMax2018() {
+    StatisticsService service = new StatisticsService();
+
+    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+    long expected = 12;
+
+    long actual = service.findMax(incomesInBillions);
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void findMax2019() {
+    StatisticsService service = new StatisticsService();
+
+    long[] incomesInBillions = {10, 5, 8, 4, 5, 3, 8, 6, 11, 14, 17};
+    long expected = 17;
+
+    long actual = service.findMax(incomesInBillions);
+
+    assertEquals(expected, actual);
+  }
 }
